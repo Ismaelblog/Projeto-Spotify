98145f7b86e82032fc705f8c5113be0912617c34
Ismaelblog <ismaelblog@hotmail.com>
2025-05-11 12:31:31 -0300

    ajuste de process do deploy

--- Front-end/api/api.js
+++ Front-end/api/api.js
@@ -1,7 +1,7 @@
 // Fetch ou Axios
 import axios from "axios";
 
-const URL = "http://localhost:3000/api";
+const URL = "https://projeto-spotify-2n5o.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
