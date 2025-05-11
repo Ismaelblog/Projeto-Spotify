53bda588c410358a281335ef82a8434ddd704da3
Ismaelblog <ismaelblog@hotmail.com>
2025-05-11 12:15:55 -0300

    UpdateDeploy1
    1 - Update Deploy_1ª Versão

--- Back-end/api/server.js
+++ Back-end/api/server.js
@@ -2,25 +2,34 @@
 // Há várias requesições: POST, GET, PUT, DELETE.
 // CRUD - Create Read Update Delete.
 
-import express, { request, response } from "express";
+import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Olá Mundo! agora não preciso ficar atualizando");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../Front-end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../Front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
